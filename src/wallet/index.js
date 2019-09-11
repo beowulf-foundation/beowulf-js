4da8de8c84ca52598675ae92625807c352c0a12d
DangAnhKiet <DangAnhKiet>
2019-09-11 17:35:25 +0700

    + Update fee

--- src/wallet/index.js
+++ src/wallet/index.js
@@ -25,7 +25,7 @@ beowulfWallet.generateMulWallet = function (amount) {
 };
 
 beowulfWallet.submitWallet = function(
-  { ownerPubkey, account, creator, creatorWif, fee = '1.00000 W' },
+  { ownerPubkey, account, creator, creatorWif, fee = '0.10000 W' },
   cb
 ) {
   let jsonMetadata = '';
@@ -68,7 +68,7 @@ beowulfWallet.accountUpdate = function(
 };
 
 beowulfWallet.submitMulWallet = function(
-  { wallet, weight_threshold, account, creator, creatorWif, fee = '1.00000 W' },
+  { wallet, weight_threshold, account, creator, creatorWif, fee = '0.10000 W' },
   cb
 ) {
   let jsonMetadata = '';

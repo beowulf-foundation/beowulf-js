160aacb85d4f0901231306cdfb36dd4ce5ae56fe
Nhut Phan <thanquan@gmail.com>
2019-11-25 17:57:30 +0700

    Format broadcast/index

--- src/broadcast/index.js
+++ src/broadcast/index.js
@@ -9,7 +9,7 @@ import beowulfAuth from '../auth';
 import { camelCase } from '../utils';
 
 const debug = newDebug('beowulf:broadcast');
-const noop = function() {}
+const noop = function() {};
 const formatter = formatterFactory(beowulfApi);
 
 const beowulfBroadcast = {};
@@ -21,43 +21,52 @@ const beowulfBroadcast = {};
  */
 
 beowulfBroadcast.send = function beowulfBroadcast$send(tx, privKeys, callback) {
-  const resultP = beowulfBroadcast._prepareTransaction(tx)
-    .then((transaction) => {
+  const resultP = beowulfBroadcast
+    ._prepareTransaction(tx)
+    .then(transaction => {
       debug(
         'Signing transaction (transaction, transaction.operations)',
-        transaction, transaction.operations
+        transaction,
+        transaction.operations,
       );
       return Promise.join(
         transaction,
-        beowulfAuth.signTransaction(transaction, privKeys)
+        beowulfAuth.signTransaction(transaction, privKeys),
       );
     })
     .spread((transaction, signedTransaction) => {
       debug(
         'Broadcasting transaction (transaction, transaction.operations)',
-        transaction, transaction.operations
+        transaction,
+        transaction.operations,
       );
-      return beowulfApi.broadcastTransactionSynchronousAsync(
-        signedTransaction
-      ).then((result) => {
-        return Object.assign({}, result, signedTransaction);
-      });
+      return beowulfApi
+        .broadcastTransactionSynchronousAsync(signedTransaction)
+        .then(result => {
+          return Object.assign({}, result, signedTransaction);
+        });
     });
 
   resultP.nodeify(callback || noop);
 };
 
-beowulfBroadcast.create = function beowulfBroadcast$create(tx, privKeys, callback) {
-  const resultP = beowulfBroadcast._prepareTransaction(tx)
-    .then((transaction) => {
+beowulfBroadcast.create = function beowulfBroadcast$create(
+  tx,
+  privKeys,
+  callback,
+) {
+  const resultP = beowulfBroadcast
+    ._prepareTransaction(tx)
+    .then(transaction => {
       debug(
         'Signing transaction (transaction, transaction.operations)',
-        transaction, transaction.operations
+        transaction,
+        transaction.operations,
       );
 
       return beowulfAuth.signTransaction(transaction, privKeys);
     })
-    .then((signedTransaction) => {
+    .then(signedTransaction => {
       return Object.assign({}, signedTransaction);
     });
 
@@ -65,46 +74,52 @@ beowulfBroadcast.create = function beowulfBroadcast$create(tx, privKeys, callbac
 };
 
 beowulfBroadcast.sign = function beowulfBroadcast$sign(tx, privKeys) {
-  const signedTransaction = beowulfAuth.signTransaction(tx, privKeys)
+  const signedTransaction = beowulfAuth.signTransaction(tx, privKeys);
   return Object.assign({}, signedTransaction);
 };
 
-beowulfBroadcast.broadcast = function beowulfBroadcast$broadcast(signedTransaction, callback) {
-  const resultP = beowulfApi.broadcastTransactionSynchronousAsync(
-    signedTransaction
-  ).then((result) => {
-    return Object.assign({}, result);
-  });
+beowulfBroadcast.broadcast = function beowulfBroadcast$broadcast(
+  signedTransaction,
+  callback,
+) {
+  const resultP = beowulfApi
+    .broadcastTransactionSynchronousAsync(signedTransaction)
+    .then(result => {
+      return Object.assign({}, result);
+    });
 
   resultP.nodeify(callback || noop);
 };
 
-beowulfBroadcast._prepareTransaction = function beowulfBroadcast$_prepareTransaction(tx) {
+beowulfBroadcast._prepareTransaction = function beowulfBroadcast$_prepareTransaction(
+  tx,
+) {
   const propertiesP = beowulfApi.getDynamicGlobalPropertiesAsync();
-  return propertiesP
-    .then((properties) => {
-      // Set defaults on the transaction
-      const chainDate = new Date(properties.time + 'Z');
-      const refBlockNum = (properties.last_irreversible_block_num - 1) & 0xFFFF;
-      return beowulfApi.getBlockAsync(properties.last_irreversible_block_num).then((block) => {
+  return propertiesP.then(properties => {
+    // Set defaults on the transaction
+    const chainDate = new Date(properties.time + 'Z');
+    const refBlockNum = (properties.last_irreversible_block_num - 1) & 0xffff;
+    return beowulfApi
+      .getBlockAsync(properties.last_irreversible_block_num)
+      .then(block => {
         const headBlockId = block.previous;
-        return Object.assign({
-          ref_block_num: refBlockNum,
-          ref_block_prefix: Buffer.from(headBlockId, 'hex').readUInt32LE(4),
-          created_time: parseInt(+(new Date()) / 1000),
-          expiration: new Date(
-            chainDate.getTime() +
-            3600 * 1000
-          ),
-        }, tx);
+        return Object.assign(
+          {
+            ref_block_num: refBlockNum,
+            ref_block_prefix: Buffer.from(headBlockId, 'hex').readUInt32LE(4),
+            created_time: parseInt(+new Date() / 1000),
+            expiration: new Date(chainDate.getTime() + 3600 * 1000),
+          },
+          tx,
+        );
       });
-    });
+  });
 };
 
 // Generated wrapper ----------------------------------------------------------
 
 // Generate operations from operations.json
-operations.forEach((operation) => {
+operations.forEach(operation => {
   const operationName = camelCase(operation.operation);
   const operationParams = operation.params || [];
 
@@ -123,70 +138,138 @@ operations.forEach((operation) => {
 
   beowulfBroadcast[`${operationName}Keys`] = buildKeys;
 
-  beowulfBroadcast[`${operationName}With`] =
-    function beowulfBroadcast$specializedSendWith(wif, options, callback) {
-      debug(`Sending operation "${operationName}" with`, {options, callback});
-      const keys = buildKeys(wif);
+  beowulfBroadcast[
+    `${operationName}With`
+  ] = function beowulfBroadcast$specializedSendWith(
+    wif,
+    options,
+    data_extension,
+    callback,
+  ) {
+    debug(`Sending operation "${operationName}" with`, { options, callback });
+    const keys = buildKeys(wif);
+    let extensions;
+    if (data_extension != null) {
+      extensions = [
+        {
+          type: 'extension_json_type',
+          value: {
+            data: data_extension,
+          },
+        },
+      ];
+    } else {
+      extensions = [];
+    }
+    return beowulfBroadcast.send(
+      {
+        extensions,
+        operations: [
+          [
+            operation.operation,
+            Object.assign(
+              {},
+              options,
+              options.json_metadata != null
+                ? {
+                    json_metadata: toString(options.json_metadata),
+                  }
+                : {},
+              useCommentPermlink && options.permlink == null
+                ? {
+                    permlink: formatter.commentPermlink(
+                      options.parent_author,
+                      options.parent_permlink,
+                    ),
+                  }
+                : {},
+            ),
+          ],
+        ],
+      },
+      keys,
+      callback,
+    );
+  };
 
-      return beowulfBroadcast.send({
+  beowulfBroadcast[operationName] = function beowulfBroadcast$specializedSend(
+    wif,
+    ...args
+  ) {
+    debug(`Parsing operation "${operationName}" with`, { args });
+    if (args.length == 6) {
+      var data_extension = null;
+    } else {
+      var data_extension = args[args.length - 2];
+    }
+    const options = operationParams.reduce((memo, param, i) => {
+      memo[param] = args[i]; // eslint-disable-line no-param-reassign
+      return memo;
+    }, {});
+    const callback = args[args.length - 1];
+    return beowulfBroadcast[`${operationName}With`](
+      wif,
+      options,
+      data_extension,
+      callback,
+    );
+  };
+
+  beowulfBroadcast[`${operationName}Multisig`] = function(wif, ...args) {
+    debug(`Parsing operation "${operationName}" with`, { args });
+    const options = operationParams.reduce((memo, param, i) => {
+      memo[param] = args[i]; // eslint-disable-line no-param-reassign
+      return memo;
+    }, {});
+    const callback = args[operationParams.length];
+    return beowulfBroadcast[`${operationName}MultisigWith`](
+      wif,
+      options,
+      callback,
+    );
+  };
+
+  beowulfBroadcast[`${operationName}MultisigWith`] = function(
+    wif,
+    options,
+    callback,
+  ) {
+    debug(`Sending operation "${operationName}" with`, { options, callback });
+    const keys = buildKeys(wif);
+
+    return beowulfBroadcast.create(
+      {
         extensions: [],
-        operations: [[operation.operation, Object.assign(
-          {},
-          options,
-          options.json_metadata != null ? {
-            json_metadata: toString(options.json_metadata),
-          } : {},
-          useCommentPermlink && options.permlink == null ? {
-            permlink: formatter.commentPermlink(options.parent_author, options.parent_permlink),
-          } : {}
-        )]],
-      }, keys, callback);
-    };
-
-  beowulfBroadcast[operationName] =
-    function beowulfBroadcast$specializedSend(wif, ...args) {
-      debug(`Parsing operation "${operationName}" with`, {args});
-      const options = operationParams.reduce((memo, param, i) => {
-        memo[param] = args[i]; // eslint-disable-line no-param-reassign
-        return memo;
-      }, {});
-      const callback = args[operationParams.length];
-      return beowulfBroadcast[`${operationName}With`](wif, options, callback);
-    };
-  
-  beowulfBroadcast[`${operationName}Multisig`] =
-    function(wif, ...args) {
-      debug(`Parsing operation "${operationName}" with`, {args});
-      const options = operationParams.reduce((memo, param, i) => {
-        memo[param] = args[i]; // eslint-disable-line no-param-reassign
-        return memo;
-      }, {});
-      const callback = args[operationParams.length];
-      return beowulfBroadcast[`${operationName}MultisigWith`](wif, options, callback);
-    };
-
-  beowulfBroadcast[`${operationName}MultisigWith`] =
-    function(wif, options, callback) {
-      debug(`Sending operation "${operationName}" with`, {options, callback});
-      const keys = buildKeys(wif);
-
-      return beowulfBroadcast.create({
-        extensions: [],
-        operations: [[operation.operation, Object.assign(
-          {},
-          options,
-          options.json_metadata != null ? {
-            json_metadata: toString(options.json_metadata),
-          } : {},
-          useCommentPermlink && options.permlink == null ? {
-            permlink: formatter.commentPermlink(options.parent_author, options.parent_permlink),
-          } : {}
-        )]],
-      }, keys, callback);
-    };
+        operations: [
+          [
+            operation.operation,
+            Object.assign(
+              {},
+              options,
+              options.json_metadata != null
+                ? {
+                    json_metadata: toString(options.json_metadata),
+                  }
+                : {},
+              useCommentPermlink && options.permlink == null
+                ? {
+                    permlink: formatter.commentPermlink(
+                      options.parent_author,
+                      options.parent_permlink,
+                    ),
+                  }
+                : {},
+            ),
+          ],
+        ],
+      },
+      keys,
+      callback,
+    );
+  };
 });
 
-const toString = obj => typeof obj === 'object' ? JSON.stringify(obj) : obj;
+const toString = obj => (typeof obj === 'object' ? JSON.stringify(obj) : obj);
 broadcastHelpers(beowulfBroadcast);
 
 Promise.promisifyAll(beowulfBroadcast);
